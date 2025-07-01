c6500efebe7db4b3ddbf2e5060e0d2f670c72c78
kennylixi <kenny.lixi@gmail.com>
2025-07-01 20:18:42 +0800

    init

---
+++ scripts.d.ts
@@ -0,0 +1,230 @@
+type MaybePromise<T> = T | Promise<T>
+
+type UseModalOptions = { component?: any } & Partial<{
+  open: boolean
+  title?: string
+  footer?: boolean
+  maxHeight?: string
+  maxWidth?: string
+  minWidth?: string
+  minHeight?: string
+  width?: string
+  height?: string
+  cancel?: boolean
+  submit?: boolean
+  cancelText?: string
+  submitText?: string
+  maskClosable?: boolean
+  onOk?: () => MaybePromise<boolean | void>
+  onCancel?: () => MaybePromise<boolean | void>
+  beforeClose?: (isOk: boolean) => MaybePromise<boolean | void>
+  afterClose?: (isOk: boolean) => void
+}>
+
+interface AutoScripts {
+  APP_TITLE: string
+  APP_VERSION: string
+  YAML: {
+    parse(text: string): any
+    stringify(obj: any): string
+  }
+  SubStoreCache?: {
+    data: Record<string, any>
+    sync: () => void
+    get(key: string): any
+    set(key: string, value: any): void
+    remove(key: string): void
+  }
+
+  alert(title: string, content: string, options?: { type?: string; markdown?: boolean }): Promise<void>
+  prompt(title: string, initialValue?: string, options?: { placeholder?: string; type?: string }): Promise<string>
+  confirm(
+    title: string,
+    content: string,
+    options?: {
+      type?: string
+      okText?: string
+      cancelText?: string
+      markdown?: boolean
+    }
+  ): Promise<boolean>
+  modal(options?: UseModalOptions): {
+    destroy: () => void
+  } & {
+    open: () => void
+    close: () => void
+    setProps: (options: UseModalOptions) => void
+    setComponent: (component: any) => void
+  }
+  picker: {
+    multi(title: string, options: Array<{ label: string; value: any }>, initialValue?: any[]): Promise<any[]>
+    single(
+      title: string,
+      options: Array<{
+        label: string
+        value: any
+        description?: string
+        background?: string
+        onSelect?: (item: { value: any }) => void
+      }>,
+      initialValue?: any[]
+    ): Promise<any>
+  }
+  message: {
+    success(msg: string, duration?: number): void
+    warn(msg: string, duration?: number): void
+    info(
+      msg: string,
+      duration?: number,
+      onClose?: () => void
+    ): {
+      id: string | number
+      update: (msg: string, type?: string) => void
+      destroy: () => void
+      success: (msg: string) => void
+      error: (msg: string) => void
+    }
+    update(id: string | number, msg: string, type?: string): void
+    destroy(id: string | number): void
+    error(msg: string): void
+  }
+
+  AbsolutePath(relativePath: string): Promise<string>
+  Copyfile(src: string, dest: string): Promise<void>
+  Makedir(path: string): Promise<void>
+  Readdir(path: string): Promise<Array<{ name: string; isDir: boolean; size: number }>>
+  FileExists(path: string): Promise<boolean>
+  Removefile(path: string): Promise<void>
+  Movefile(src: string, dest: string): Promise<void>
+  Readfile(path: string, options?: { Mode?: 'Binary' | 'Text' }): Promise<string>
+  Writefile(path: string, content: string, options?: { Mode?: 'Binary' | 'Text' }): Promise<void>
+  UnzipGZFile(gzPath: string, destPath: string): Promise<void>
+  UnzipZIPFile(zipPath: string, destPath: string): Promise<void>
+
+  ListServer(): Promise<string[]>
+  StopServer(id: string): Promise<void>
+  StartServer(
+    address: string,
+    serverId: string,
+    onRequest: (
+      req: {
+        url: string
+        method: string
+        headers: Record<string, string>
+        body: string
+      },
+      res: {
+        end: (status: number, headers: Record<string, string>, body: string, options?: { Mode: 'Binary' | 'Text' }) => void
+      }
+    ) => void,
+    options?: {
+      StaticPath?: string
+      UploadPath?: string
+      UploadRoute?: string
+      MaxUploadSize?: number
+    }
+  ): Promise<{ close: () => Promise<void> }>
+
+  ProcessInfo(pid: number): Promise<string>
+  KillProcess(pid: number, timeout?: number): Promise<void>
+  Exec(cmd: string, args?: string[], options?: { convert?: boolean }): Promise<string>
+  ExecBackground(
+    cmd: string,
+    args: string[],
+    onOut: (out: string) => void,
+    onExit: () => void,
+    options?: { env?: Record<string, string>; convert?: boolean }
+  ): Promise<number>
+
+  Request: any
+  HttpGet(
+    url: string,
+    headers?: Record<string, string>,
+    options?: { Insecure?: boolean; Redirect?: boolean }
+  ): Promise<{ status: number; headers: Record<string, string>; body: any }>
+  HttpPost(
+    url: string,
+    headers: Record<string, string>,
+    data: any,
+    options?: { Timeout?: number; Insecure?: boolean; Redirect?: boolean }
+  ): Promise<{ status: number; headers: Record<string, string>; body: any }>
+  HttpDelete(
+    url: string,
+    headers: Record<string, string>,
+    options?: { Insecure?: boolean; Redirect?: boolean }
+  ): Promise<{ status: number; headers: Record<string, string>; body: any }>
+  HttpPatch(url: string, headers: Record<string, string>, data: any): Promise<{ status: number; headers: Record<string, string>; body: any }>
+  Requests(options: {
+    method: string
+    url: string
+    headers?: Record<string, string>
+    body?: string
+    autoTransformBody?: boolean
+    options?: {
+      Insecure?: boolean
+      Redirect?: boolean
+    }
+  }): Promise<{ status: number; body: any }>
+  Download(
+    url: string,
+    path: string,
+    headers?: Record<string, string>,
+    progressCallback?: (progress: number, total: number) => void,
+    options?: {
+      Method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+    }
+  ): Promise<void>
+
+  GetInterfaces(): Promise<string[]>
+  CheckPermissions(): Promise<boolean>
+  RestartApp(): Promise<void>
+  BrowserOpenURL(url: string): void
+  WindowReloadApp(): Promise<void>
+
+  ClipboardSetText(text: string): Promise<void>
+  ClipboardGetText(): Promise<string>
+
+  ValidateCron(expression): Promise<boolean>
+
+  OpenMMDB(path: string, id: string): Promise<void>
+  CloseMMDB(path: string, id: string): Promise<void>
+  QueryMMDB(path: string, ip: string, type?: 'ASN' | 'AnonymousIP' | 'City' | 'ConnectionType' | 'Country' | 'Domain' | 'Enterprise'): Promise<any> // Define return type if known
+
+  useAppStore(): any
+  useKernelApiStore(): any
+  usePluginsStore(): any
+  useAutoScriptsStore(): any
+  useRulesetsStore(): any
+  useSubscribesStore(): any
+  useEnvStore(): any
+  useAppSettingsStore(): any
+  useProfilesStore(): any
+  useScheduledTasksStore(): any
+
+  setIntervalImmediately(fn: () => void, delay: number): number
+  formatRelativeTime(dateString: string): string
+  base64Decode(encoded: string): string
+  base64Encode(text: string): string
+  deepClone<T>(obj: T): T
+  deepAssign<T, U>(target: T, source: U): T & U
+  asyncPool: <T>(poolLimit: number, array: T[], iteratorFn: (item: T, array: T[]) => Promise<any>) => Promise<any[]>
+  sampleID(): string
+  isValidIPv4(ip: string): boolean
+  generateConfig(profile: any, stable?: boolean): Promise<any>
+  formatBytes(bytes: number): string
+  formatDate(date: string | number, format: string)
+  handleUseProxy(group: any, proxy: { name: string }): Promise<void>
+  debounce(fn: (...args: any[]) => void, delay: number): (...args: any[]) => void
+  getKernelFileName(isAlpha: boolean): Promise<string>
+  getUserAgent(): Promise<string>
+  getGitHubApiAuthorization?(): string
+  sleep(ms: number): Promise<void>
+  ignoredError<T extends (...args: any[]) => any>(fn: T, ...args: Parameters<T>): Promise<ReturnType<T> | undefined>
+  exitApp(): Promise<void>
+}
+
+declare namespace globalThis {
+  var AutoScripts: AutoScripts
+  var AutoScript: any
+  var Vue: typeof import('vue')
+}
---
+++ scripts/Generic/script-aliyunpan-signin.js
@@ -0,0 +1,137 @@
+/**
+ * 本插件参考项目：https://github.com/mrabit/aliyundriveDailyCheck/blob/master/autoSignin.js
+ */
+
+const UA =
+  'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5'
+
+// 使用 refresh_token 更新 access_token
+async function updateAccesssToken(refreshToken) {
+  const { body } = await AutoScripts.HttpPost(
+    'https://auth.aliyundrive.com/v2/account/token',
+    {
+      'User-Agent': UA,
+      'Content-Type': 'application/json'
+    },
+    {
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken
+    }
+  )
+  const { code, message, refresh_token, access_token } = body
+  if (code) {
+    throw message
+  }
+  return { refresh_token, access_token }
+}
+
+//签到列表
+async function sign_in(access_token) {
+  const { body } = await AutoScripts.HttpPost(
+    'https://member.aliyundrive.com/v1/activity/sign_in_list?_rx-s=mobile',
+    {
+      'User-Agent': UA,
+      'Content-Type': 'application/json',
+      Authorization: access_token
+    },
+    {
+      isReward: false
+    }
+  )
+
+  if (body.code) {
+    throw body.message
+  }
+
+  const { signInLogs, signInCount } = body.result
+
+  // 未领取奖励列表
+  // const rewards = signInLogs.filter((v) => v.status === 'normal' && !v.isReward)
+
+  // for await (reward of rewards) {
+  //   await getReward(access_token, reward.day).catch((err) => {
+  //     console.log('领取奖励失败：', err)
+  //   })
+  // }
+
+  return `累计签到${signInCount}天。`
+}
+
+// 领取奖励
+async function getReward(access_token, signInDay) {
+  const { body } = await AutoScripts.HttpPost(
+    'https://member.aliyundrive.com/v1/activity/sign_in_reward?_rx-s=mobile',
+    {
+      'User-Agent': UA,
+      'Content-Type': 'application/json',
+      authorization: access_token
+    },
+    {
+      signInDay
+    }
+  )
+
+  if (!body.success) {
+    throw body.message
+  }
+}
+
+const SignIn = async () => {
+  if (!AutoScript.RefreshTokenList?.length) throw '未提供任何账号'
+
+  const TOKEN_CONFIG = 'data/third/aliyunpan-signin/config.json'
+
+  const tokenConfig = (await AutoScripts.ignoredError(AutoScripts.Readfile, TOKEN_CONFIG)) || '{}'
+  const TokenMap = JSON.parse(tokenConfig)
+
+  async function refreshAccessToken(token, refreshToken) {
+    const { refresh_token, access_token } = await updateAccesssToken(refreshToken)
+    TokenMap[token] = {
+      refresh_token,
+      access_token
+    }
+  }
+
+  const res = []
+
+  for (let i = 0; i < AutoScript.RefreshTokenList.length; i++) {
+    const token = AutoScript.RefreshTokenList[i]
+    const { refresh_token: latestRefreshToken = token, access_token: latestAccessToken } = TokenMap[token] || {}
+
+    try {
+      if (!latestAccessToken) {
+        await refreshAccessToken(token, latestRefreshToken)
+      }
+      if (TokenMap[token]?.access_token) {
+        try {
+          const days = await sign_in(TokenMap[token].access_token)
+          res.push(`账号【${token.slice(0, 8)}】签到成功，` + days)
+        } catch (error) {
+          await refreshAccessToken(token, TokenMap[token]?.refresh_token || token)
+          const days = await sign_in(TokenMap[token].access_token)
+          res.push(`账号【${token.slice(0, 8)}】签到成功，` + days)
+        }
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  await AutoScripts.Writefile(TOKEN_CONFIG, JSON.stringify(TokenMap, null, 2))
+
+  return res
+}
+
+const onRun = async () => {
+  const res = await SignIn()
+  AutoScripts.alert('签到信息', res.join('\n'))
+}
+
+const onTask = async () => {
+  try {
+    const res = await SignIn()
+    return res.join('\n')
+  } catch (error) {
+    return error
+  }
+}
